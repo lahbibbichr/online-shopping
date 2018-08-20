7406f237d2afa15989d3c9caa46986b9fc9519bf
lahbibbichr <lahbib.bichr@gmail.com>
2018-08-20 22:38:15 +0100

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/l2r/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.l2r.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
